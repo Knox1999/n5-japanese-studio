1a70ed5b50dc6df4762c44a0f7b27e1fa33aa997
Knox1999 <libertychemicals3@gmail.com>
2026-08-29 23:50:19 +0600

    Load final shell correction layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ import '@/styles/v62-dashboard-context.scss';
 import '@/styles/v63-brand-experience.scss';
 import '@/styles/v64-learning-labs.scss';
 import '@/styles/v65-production-qa.scss';
+import '@/styles/v66-shell-correction.scss';
 
 const SITE = 'https://knox1999.github.io/n5-japanese-studio/';
 
